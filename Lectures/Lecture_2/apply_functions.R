4d1155a2e6d633224b5e504d9354f0a8ddb6da76
ogencoglu <oguzhan.gencoglu@tut.fi>
2016-05-03 13:04:57 +0300

    new code

--- Lectures/Lecture_2/apply_functions.R
+++ Lectures/Lecture_2/apply_functions.R
@@ -1,6 +1,6 @@
-# R Lecture to VTT - Lecture 1
+# R Lecture to VTT - Lecture 2
 # Author                : Oguzhan Gencoglu
-# Latest Version        : 27.04.2016
+# Latest Version        : 03.05.2016
 # Contact               : oguzhan.gencoglu@tut.fi
 
 # ---------------- Apply Functions ----------------
@@ -13,16 +13,10 @@ my_list2 <- 1:5
 lapply(my_list2, runif)
 lapply(my_list2, runif, min = 3, max = 7)
 
-my_list3 <- list( a = matrix(1:4, 2, 2), b = matrix(1:6, 3, 2))
-lapply(my_list3, function(my_func) my_func[,2])
-
 # sapply simplifies the result
 
 # tapply is used to apply a function to a subset of the input
-my_list4 <- c(rnorm(10), runif(10), rnorm(10))
-index <- gl(3,10)
-tapply(my_list4, index, mean)
-tapply(my_list4, index, range)
-
-result <- lapply(split(my_list4, index), mean)
-result
+my_list3 <- c(rnorm(10), runif(10), rnorm(10))
+index <- gl(3,10) # generate factor levels
+tapply(my_list3, index, mean)
+tapply(my_list3, index, range)
