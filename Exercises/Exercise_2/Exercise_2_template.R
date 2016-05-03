202ec31233163f0431d10011b165d445a2556308
ogencoglu <oguzhan.gencoglu@tut.fi>
2016-05-04 02:06:02 +0300

    Exercise 2 added

---
+++ Exercises/Exercise_2/Exercise_2_template.R
@@ -0,0 +1,40 @@
+# R Lecture to VTT - Exercise 2 Template
+
+# required libraries
+library(xlsx)
+library(lattice)
+library(ggplot2)
+
+# --------------------- TASK 1 --------------------------
+# Read the data
+        ### YOUR CODE HERE ###
+
+# Create lattice shingles using equal.counts. Use the columns DA and Area.
+# Take the logarithm of Area column.
+        ### YOUR CODE HERE ###
+        ### YOUR CODE HERE ###
+
+# Plot PA500 and P columns conditioned on previously created sinhles using 
+# lattice xyplot function. Set plot title, x-axis title and y-axis title
+# with 'main', 'ylab' and 'xlab' arguments, respectively. Set the coloring 
+#using the 'col' argument.
+        ### YOUR CODE HERE ###
+        ### YOUR CODE HERE ###
+
+
+# --------------------- TASK 2 --------------------------
+# Use ggplot function to create a canvas. Add a scatter plot by using
+# geom_point. Adding an 'aes' field with the 'colour' argument inside
+# the geom_point element will color the points with respect to desired column.
+# geom_smooth function adds a least squares fit.
+# ggtitle can add a title text.
+        ### YOUR CODE HERE ###
+        ### YOUR CODE HERE ###
+
+# --------------------- TASK 3 --------------------------
+# create dataframe
+        ### YOUR CODE HERE ###
+
+# Use geom_hex and geom_density2d functions.
+        ### YOUR CODE HERE ###
+        ### YOUR CODE HERE ###
